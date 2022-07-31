81446353951c75179f31c83a3bcf0ed165098893
Gabriel Forneck <gabrielforneck2004@gmail.com>
2022-07-31 14:33:59 -0300

    Starting class SubtitlesList.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -18,18 +19,42 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter fmt =  DateTimeFormatter.ofPattern("HH:mm:ss");
 		
-		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main] Project 'str-tool' by Gabriel Forneck");
+		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main/INFO] Project 'str-tool' by Gabriel Forneck");
 		
 		System.out.println();
-		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main] Enter the path of the .srt file:");
-		String originalSTRPath = sc.nextLine();
+		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main/INFO] Enter the path of the .srt file:");
+		String originalSRTPath = sc.nextLine();
 		
-		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main] Reading...");
+		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main/INFO] Reading...");
 		BufferedReader br = null;
 		FileReader fr = null;
 		
+		try {
+			fr = new FileReader(originalSRTPath);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main/ERROR] An error has occurred while reading the file:");
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if (br != null)
+						br.close();
+				if (fr != null)
+						fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		
 		sc.close();
-		System.out.println("[System/main] Program ended successfully");
+		System.out.println("[" + fmt.format(LocalDateTime.now()) + "] [System/main/END] Program ended successfully");
 	}
 
 }
